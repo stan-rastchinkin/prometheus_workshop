eed4bd5feb785c922eb1a8434b681b8ea2aba57a
Stanislav Rastchinkin <stanislav.rastchinkin@wunderflats.com>
2023-11-17 18:20:08 +0100

    prepare task-1/instrumentation

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { faker } from '@faker-js/faker';
+// TODO: Import Histogram metric
+import promClient, { Counter /*, Histogram*/ } from 'prom-client';
 
 import { genres, getRandomGenre } from './get-data.js';
 
@@ -15,8 +17,34 @@ const waitArbitraryTime = (
   return new Promise((res) => setTimeout(res, delay));
 };
 
+// #########################
+// # Prometheus Instrumentation
+// #########################
+
+const genresRequestsCounter = new Counter({
+  name: 'genres_requests_counter',
+  help: 'Number of requests to /genres route',
+});
+
+// TODO: create a new Histogram for tracking the duration of requests
+// to the /recommendation route
+
+// const recommendationRequestDurationHistogram = new Histogram({
+//   name: 'recommendation_duration_seconds',
+//   help: 'Duration of requests to /recommendation in seconds',
+// });
+
+// #########################
+// # Application Routes
+// #########################
+
 router.get('/recommendation', async (_req, res) => {
+  // TODO: use the built-in timers functionality
+
+  // const stopTimer = recommendationRequestDurationHistogram.startTimer();
   await waitArbitraryTime(100, 2000);
+  // stopTimer();
+
   res.json({
     artist: faker.person.fullName(),
     genre: getRandomGenre(),
@@ -26,11 +54,22 @@ router.get('/recommendation', async (_req, res) => {
 
 router.get('/genres', async (_req, res) => {
   await waitArbitraryTime(50, 1000);
+  genresRequestsCounter.inc();
   res.json(genres);
 });
 
 router.get('/', (_req, res) => {
   res.redirect('/recommendation');
+});
+
+// #########################
+// # Metrics
+// #########################
+
+router.get('/metrics', async (_req, res) => {
+  const register = promClient.register;
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
 });
 
 export default router;
